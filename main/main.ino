da209fc926f18f2f4231b8e0bfbaf8a09b5b9639
Mike <125614854+E64b@users.noreply.github.com>
2023-09-21 02:30:08 +0300

    v2.0

--- main/main.ino
+++ main/main.ino
@@ -5,74 +5,78 @@
 #define CLK 2
 #define DIO 3
 
-uint64_t freq;
-uint64_t periodns;
-volatile uint16_t cnt_ovf = 0;
-inline __attribute__((always_inline))
+uint32_t freq;
+uint32_t periodns;
 float Timer;
-uint64_t peri = 0;
+bool Disp = false;
+bool Phase = false;
 GyverTM1637 disp(CLK, DIO);
 AD9833 gen(FNC_PIN);
 
-void setup() 
+/*Меняем частоту*/
+void CalcFreq()
 {
-  gen.Begin();
-  gen.ApplySignal(SINE_WAVE, REG0, freq);
-  gen.EnableOutput(true);
-  disp.clear();
-  disp.brightness(7);
-  disp.point(0);
-  /*Первичная инициализация таймера*/
-  TCCR1A = TCCR1B = TCNT1 = cnt_ovf = 0;
-  TIFR1 |= (1 << TOV1);
-  TIMSK1 = (1 << TOIE0);
-  TCCR1B = (1 << CS10);
-  freq = 30000;
-}
-
-ISR(TIMER1_OVF_vect) 
-{
-  cnt_ovf++;
-}
-
-
-/*Считаем период текущей частоты и устанавливаем паузу перед следующей*/
-void PeriodDelay()
-{
-  peri = 0;
-  Timer = 0; //Обнуляем переменную   
-  periodns = round(1000000000 / freq);//Считаем период в наносекундах
-  TCCR1A = TCCR1B = TCNT1 = cnt_ovf = 0; //Сбрасываем таймер
-  TCCR1B = (1 << CS10); //Запускаем таймер
-  peri = periodns / 2;
-  /*Сидим в этом цикле пока не пройдет полупериод*/
-   while (Timer <= (peri))
-   { 
-    gen.EnableOutput(false);  //Выключаем передачу сигнала 
-    uint32_t count = TCNT1 - 2; // минус два такта на действия
-    count += ((uint32_t)cnt_ovf * 0xFFFF);  // с учетом переполнений
-    Timer = Timer + (count * (1000000000.0f / F_CPU)); //Считаем время
-  }
-  TCCR1B = 0; // остановить таймер
-}
-
-
-/*Выполняем все по кругу*/
-void loop() 
-{
-  /*Считаем частоту*/
-  if ((freq <= 999000) and (freq >= 30000))
+    if ((freq <= 999000) and (freq <=30000))
     {
-      freq = freq + 200;
+        freq = freq + 200;
+    }
+    else
+    {
+        freq = 30000;
+    }
+}
+
+/*Если частота обновилась выводим на дисплей*/
+void Display()
+{
+    if (Disp)
+    {
+        disp.clear();
+        disp.displayInt(freq / 1000);
+    }
+}
+/*Считаем период текущей частоты*/
+void Period()
+{
+    periodns = round(1000000000.0f / freq);//Считаем период в наносекундах
+    periodns = periodns / 2; //считаем полупериод
+}
+
+/*Отправляем частоту */
+void SendSignal()
+{
+    gen.ApplySignal(HALF_SQUARE_WAVE, REG0, freq);
+    gen.EnableOutput(true);
+    if (Phase == true)
+    {
+        gen.IncrementPhase(REG1, 0);
+        Phase = false;
     }
     else 
     {
-      freq = 30000;
+        gen.IncrementPhase(REG1, 180);
+        Phase = true;
     }
-/*Отправляем частоту и выводим на дисплей*/
-    gen.ApplySignal(SQUARE_WAVE, REG0, freq);
-    gen.EnableOutput(true); 
+}
+
+void setup()
+{   
+    /*Инициализация AD9833*/
+    gen.Begin();
+    gen.ApplySignal(HALF_SQUARE_WAVE, REG0, freq);
+    gen.EnableOutput(true);
+
+    /*Инициализация дисплея*/
     disp.clear();
-    disp.displayInt(freq / 1000);
-    PeriodDelay();
+    disp.brightness(7);
+    disp.point(0);        
+}
+
+/*Выполняем все по кругу*/
+void loop()
+{
+    CalcFreq();
+    Display();
+    Period();
+    SendSignal();   
 }
